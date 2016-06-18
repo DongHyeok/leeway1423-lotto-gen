eb51e38f64adfac8e5116ca3cd2da1369dac43fc
LEEWAY <leeway1423@gmail.com>
2016-06-18 17:01:07 +0900

    로또 번호 생성 함수 작성

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 
-n generate() {  var numbers = [];  for(var i=0; i<6; i++) {    numbers.push(Math.floor(Math.random() * 45));  }  return numbers; } 
+function generate() { 
+var numbers = []; 
+for(var i=0; i<6; i++) {  
+numbers.push(Math.floor(Math.random() * 45 + 1));  
+}  
+return numbers; } 
