c3f3e27ce7708fd03fae4b8cf55fba4238a41911
LEEWAY <leeway1423@gmail.com>
2016-06-18 17:19:11 +0900

    머지 충동 문제 해결

--- js/app.js
+++ js/app.js
@@ -9,10 +9,12 @@ numbers.push(number);
 } 
 }  return numbers; 
 } 
-// ==배열 안에 number가 포함되어 있는지 확인== 
+/* 배열 안에 number가 포함되어 있는지 확인*/
 function contains(numbers, number) { 
 for(var i=0; i<numbers.length; i++) { 
 if(numbers[i] == number) 
 return true; 
 }  
-return false; }
+return false; 
+}
+
