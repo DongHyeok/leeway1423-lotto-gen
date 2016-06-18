d27a1ad098a2047a9418dc647b3a63b80a5e2ced
LEEWAY <leeway1423@gmail.com>
2016-06-18 17:16:36 +0900

    주석 수정(master)

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 
-n generate() {  var numbers = [];  for(var i=0; i<6; i++) {    var number = Math.floor(Math.random() * 45) + 1;    if(contains(numbers, number)) {      i--;    } else {      numbers.push(number);    }  }  return numbers; } // 배열 안에 number가 포함되어 있는지 확인 function contains(numbers, number) {  for(var i=0; i<numbers.length; i++) {    if(numbers[i] == number) return true;  }  return false; }
+function generate() {
+var numbers = []; 
+for(var i=0; i<6; i++) { 
+var number = Math.floor(Math.random() * 45) + 1;   
+if(contains(numbers, number)) { 
+i--;    } else { 
+numbers.push(number);  
+} 
+}  return numbers; 
+} 
+// ==배열 안에 number가 포함되어 있는지 확인== 
+function contains(numbers, number) { 
+for(var i=0; i<numbers.length; i++) { 
+if(numbers[i] == number) 
+return true; 
+}  
+return false; }
