5517dae17d95ee24e3fcdb9de98cd0fb7d2bef0a
LEEWAY <leeway1423@gmail.com>
2016-06-18 17:13:51 +0900

    버그 수정 - 중복된 번호 문제

--- js/app.js
+++ js/app.js
@@ -1,7 +1,2 @@
 
-function generate() { 
-var numbers = []; 
-for(var i=0; i<6; i++) {  
-numbers.push(Math.floor(Math.random() * 45 + 1));  
-}  
-return numbers; } 
+n generate() {  var numbers = [];  for(var i=0; i<6; i++) {    var number = Math.floor(Math.random() * 45) + 1;    if(contains(numbers, number)) {      i--;    } else {      numbers.push(number);    }  }  return numbers; } // 배열 안에 number가 포함되어 있는지 확인 function contains(numbers, number) {  for(var i=0; i<numbers.length; i++) {    if(numbers[i] == number) return true;  }  return false; }
